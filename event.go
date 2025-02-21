053d18ddca9315e1560d77f8865a902658e02713
rowanmurrayjdtt070223 <rowanmurrayjdtt070223@gmail.com>
2025-02-21 11:34:44 +0800

    Modify event.go

--- event.go
+++ event.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Organization table collection after.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
